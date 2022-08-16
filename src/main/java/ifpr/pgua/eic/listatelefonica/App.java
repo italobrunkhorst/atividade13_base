869223562fd80fb89f79ac59d03f8bae52fab919
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-08-16 11:02:49 -0300

    corrigindo utils

--- src/main/java/ifpr/pgua/eic/listatelefonica/App.java
+++ src/main/java/ifpr/pgua/eic/listatelefonica/App.java
@@ -1,23 +1,17 @@
 package ifpr.pgua.eic.listatelefonica;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 import ifpr.pgua.eic.listatelefonica.controllers.JanelaPrincipal;
 import ifpr.pgua.eic.listatelefonica.utils.BaseAppNavigator;
 import ifpr.pgua.eic.listatelefonica.utils.ScreenRegistry;
+import ifpr.pgua.eic.listatelefonica.utils.ScreenRegistryFXML;
+
+
 
 /**
  * JavaFX App
  */
 public class App extends BaseAppNavigator {
 
-    public static final ScreenRegistry PRINCIPAL=new ScreenRegistry(App.class, "principal.fxml", o->new JanelaPrincipal());
     
 
     public static void main(String[] args) {
@@ -25,21 +19,22 @@ public class App extends BaseAppNavigator {
     }
 
     @Override
-    public ScreenRegistry getHomeFXML() {
+    public String getHome() {
         // TODO Auto-generated method stub
-        return PRINCIPAL;
+        return "PRINCIPAL";
     }
 
-    @Override
-    public Parent getHomeParent() {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
     @Override
     public String getAppTitle() {
         // TODO Auto-generated method stub
         return "Lista Telefônica";
+    }
+
+    @Override
+    public void registrarTelas() {
+        registraTela("PRINCIPAL",new ScreenRegistryFXML(App.class, "principal.fxml", o->new JanelaPrincipal()));
+    
     }
 
 }
